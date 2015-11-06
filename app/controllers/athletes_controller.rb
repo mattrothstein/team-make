6de73fa1cd13d91ca148c02dd9a5bde75176ce8d
cartagena <jcartagena93@gmail.com>
2015-11-05 19:39:11 -0500

    completed athlete sign up process with validations and error messages

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -3,16 +3,17 @@ class AthletesController < ApplicationController
   end
 
   def new
+    @athlete = Athlete.new
   end
 
   def create
-    # @athlete = Athlete.find(athlete_params)
+    @athlete = Athlete.new(athlete_params)
 
-    # if @athlete.save
-    #   render 'show'
-    # else
-    #   render 'new'
-    # end
+    if @athlete.save
+      redirect_to @athlete
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -22,12 +23,13 @@ class AthletesController < ApplicationController
   end
 
   def show
+    @athlete = Athlete.find(params[:id])
   end
 
   def destroy
   end
 
-  # def athlete_params
-  #   params.require(:athlete).permit(:name, :email, :user_name, :password, :password_confirmation)
-  # end
+  def athlete_params
+    params.require(:athlete).permit(:name, :email, :user_name, :dob, :password, :password_confirmation)
+  end
 end

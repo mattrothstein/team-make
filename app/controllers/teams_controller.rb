240b387b2398df77d9bb1649e17bd5db4efaa2ec
Matt <matthewrothstein@hotmail.com>
2015-11-05 19:40:16 -0500

    correcting team create action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,9 +1,14 @@
 class TeamsController < ApplicationController
+  before_action :set_club
+
   def index
+  end
+  def new
+    @team = current_club.teams.build
   end
 
   def create
-    @team = Team.new(team_params)
+    @team = @club.teams.build(team_params)
     if @team.save
       redirect_to 'club/show'
     else
@@ -28,6 +33,8 @@ class TeamsController < ApplicationController
   def team_params
     params.require(:team).permit(:name, :age, :coach, :roster_size, :club_id)
   end
-
+  def set_club
+    current_club
+  end
 
 end

88d37455b29abd4486f79cefbcaa9e3fc344552d
Daniel Munoz <lostlucidity@gmail.com>
2015-11-05 22:15:47 -0500

    commented out controller tests, fixed some field names not changed from corrected migrations, ran set_up method for each athlete tests and fixed tests that failed to pass. Also did some Athlete controller test.

--- test/controllers/athletes_controller_test.rb
+++ test/controllers/athletes_controller_test.rb
@@ -1,30 +1,35 @@
 require 'test_helper'
 
 class AthletesControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
+  setup do
+    @athlete = athletes(:one)
+  end
 
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:athletes)
+  end
 
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
-  # test "should get edit" do
-  #   get :edit
-  #   assert_response :success
-  # end
+  test "should create athlete" do
+    post :create, athlete: { name: @athlete.name, email: @athlete.email, dob: @athlete.dob, user_name: @athlete.user_name, password: @athlete.password_digest, password_confirmation: @athlete.password_digest}
+    assert_response :success
+  end
 
-  # test "should get update" do
-  #   get :update
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    get :edit, id: @athlete
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch :update, id: @athlete, athlete: { name: @athlete.name, email: @athlete.email, dob: @athlete.dob, user_name: @athlete.user_name, password: @athlete.password_digest, password_confirmation: @athlete.password_digest}
+    assert_response :success
+  end
 
   # test "should get show" do
   #   get :show

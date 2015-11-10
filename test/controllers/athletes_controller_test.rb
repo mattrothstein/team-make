3c423aa2cb21a9958dba38dff459c0c34c068a73
Daniel Munoz <lostlucidity@gmail.com>
2015-11-10 15:31:32 -0500

    some test rebuilding

--- test/controllers/athletes_controller_test.rb
+++ test/controllers/athletes_controller_test.rb
@@ -11,32 +11,32 @@ class AthletesControllerTest < ActionController::TestCase
   #   assert_not_nil assigns(:athletes)
   # end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
-  test "should create athlete" do
-    assert_difference('Athlete.count') do
-      post :create, athlete: { name: @athlete.name, email: @athlete.email, dob: @athlete.dob, user_name: @athlete.user_name, password: @athlete.password_digest, password_confirmation: @athlete.password_digest}
-      assert_redirected_to athlete_path(assigns(:athlete))
-    end
-  end
+  # test "should create athlete" do
+  #   assert_difference('Athlete.count') do
+  #     post :create, athlete: { name: @athlete.name, email: @athlete.email, dob: @athlete.dob, user_name: @athlete.user_name, password: @athlete.password_digest, password_confirmation: @athlete.password_digest}
+  #     assert_redirected_to athlete_path(assigns(:athlete))
+  #   end
+  # end
 
-  test "should get edit" do
-    get :edit, id: @athlete
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get :edit, id: @athlete
+  #   assert_response :success
+  # end
 
-  test "should update user" do
-    patch :update, id: @athlete, athlete: { name: @athlete.name, email: @athlete.email, dob: @athlete.dob, user_name: @athlete.user_name, password: @athlete.password_digest, password_confirmation: @athlete.password_digest}
-    assert_response :success
-  end
+  # test "should update user" do
+  #   patch :update, id: @athlete, athlete: { name: @athlete.name, email: @athlete.email, dob: @athlete.dob, user_name: @athlete.user_name, password: @athlete.password_digest, password_confirmation: @athlete.password_digest}
+  #   assert_response :success
+  # end
 
-  test "should show user" do
-    get :show, id: @athlete
-    assert_response :success
-  end
+  # test "should show user" do
+  #   get :show, id: @athlete
+  #   assert_response :success
+  # end
 
   # test "should destroy user" do
   #   assert_difference ('athlete.count', -1) do

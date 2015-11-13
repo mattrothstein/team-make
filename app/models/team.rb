29a1c1bec8fab183f264c269777854e3bf21ffc6
Daniel Munoz <lostlucidity@gmail.com>
2015-11-12 19:53:18 -0500

    added validations to some models

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,9 @@
 class Team < ActiveRecord::Base
   belongs_to :season
   has_many :spots
+
+  validates :age_group, presence: true, numericality: { only_integer: true, greater_than: 9, less_than: 18 }
+  validates :roster_size, presence: true
+  validates :name, presence:true
+  validates :coach, presence:true
 end
